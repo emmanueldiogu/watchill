ec019a60779601dd3692456d981a0b51e60cd94f
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-04-01 14:51:46 -0400

    change movies list

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,30 +2,32 @@ import {useEffect, useState} from "react";
 
 function Movies() {
 
+    
+
     const [movieList, setMovieList] = useState([]);
 
     const getMovies = () => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_MOVIE_API_KEY}`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_MOVIE_API_KEY}&append_to_response=videos,images`)
         .then(res => res.json())
         .then(json => setMovieList(json.results))
     }
-
     useEffect(() => {
         getMovies();
     }, [])
 
-    console.log(movieList);
-
+    console.table(movieList);
     
     return (
         <div>
             {movieList.map((movie, index) => {
                 return (
-                    <img key={index}
-                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path
-                }`} 
-                    alt={`${movie.title}`} 
-                    />
+                    <div key={index}>
+                        <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path
+                        }`}
+                            alt={`${movie.title}`}
+                        />
+                        <h2>{movie.title}</h2>
+                    </div>
                 );
             })}
         </div>
